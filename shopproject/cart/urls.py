034877c6844e1ac1bfbc3a21df63f37678d5994e
HariKrishnanS21 <harikrishnansreenivasan@gmail.com>
2022-10-12 19:33:27 +0530

    added

---
+++ shopproject/cart/urls.py
@@ -0,0 +1,17 @@
+
+from django.urls import path
+
+from . import views
+app_name='cart'
+urlpatterns = [
+
+    path('',views.home,name='home'),
+    path('search/',views.search,name='search'),
+    path('cart/',views.cart_details,name='cart_details'),
+    path('add/<int:id>/',views.add_cart,name='add_cart'),
+    path('remove/<int:id>/',views.item_remove,name='item_remove'),
+    path('delete/<int:id>/',views.remove,name='remove'),
+    path('<slug:c_slug>/',views.catpage,name='pro_category'),
+    path('<slug:c_slug>/<slug:p_slug>/',views.prodetails,name='prodetails'),
+
+]
